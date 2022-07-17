bd199a9c5a51e3f7b842b983746f16819e844a5d
Matt Burnett <mburnett@tutanota.de>
2022-07-17 16:44:50 +0200

    NavBar ending at 3:26:21

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { NavBar } from "../components/NavBar";
 
 const Index = () => (
-  <div>Hello World</div>
-)
+  <>
+    <NavBar />
+    <div>Hello World</div>
+  </>
+);
 
-export default Index
+export default Index;
---
+++ src/pages/login.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box, Button } from "@chakra-ui/react";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+interface registerProps {}
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button mt={4} type="submit" isLoading={isSubmitting} color="teal">
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
