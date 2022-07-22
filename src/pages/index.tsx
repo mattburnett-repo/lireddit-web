dff375cc6cb8e81fcc18d6565416cec5ce8c8feb
Matt Burnett <mburnett@tutanota.de>
2022-07-22 19:10:14 +0200

    Global Error Handling ending at 6:24:15

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
+import { Link } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
+import { Layout } from "../components/Layout";
 import { NavBar } from "../components/NavBar";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/creatUrqlClient";
+import NextLink from "next/link";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <NextLink href="/create-post">
+        <Link>create post</Link>
+      </NextLink>
+
       <div>Hello World</div>
       <br />
       {!data ? (
@@ -15,7 +21,7 @@ const Index = () => {
       ) : (
         data.posts.map((p) => <div key={p.id}>{p.title}</div>)
       )}
-    </>
+    </Layout>
   );
 };
 
