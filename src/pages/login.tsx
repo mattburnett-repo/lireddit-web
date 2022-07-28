14f2a2a8d7fcecdb775d3fa9fb476be06bfddab2
Matt Burnett <mburnett@tutanota.de>
2022-07-28 15:14:56 +0200

    Next.js Query params ending at 6:31:26

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,8 +10,6 @@ import { createUrqlClient } from "../utils/creatUrqlClient";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
 
-interface registerProps {}
-
 const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
   const [, login] = useLoginMutation();
@@ -25,7 +23,11 @@ const Login: React.FC<{}> = ({}) => {
           if (response.data?.login.errors) {
             setErrors(toErrorMap(response.data.login.errors));
           } else if (response.data?.login.user) {
-            router.push("/");
+            if (typeof router.query.next === "string") {
+              router.push(router.query.next);
+            } else {
+              router.push("/");
+            }
           }
         }}
       >
